c43954dfe0e8d50dfb7e0ad7a6950509cdb676b8
Kenneth Endfinger <kaendfinger@gmail.com>
2014-08-09 17:36:35 -0400

    Added Console Class and Improved the Prompt

--- lib/src/util/console.dart
+++ lib/src/util/console.dart
@@ -26,17 +26,56 @@ String prompt(String prompt, {bool secret: false}) {
     }
     
     if (char == '\b' || char == '\x7f' || char == '\x1b\x7f' || char == '\x1b\b') {
-      data = data.isNotEmpty ? data.substring(0, data.length - 1) : "";
-      stdout.write("\n\x1b[F\x1b[J${prompt}${secret ? strs.repeat("*", data.length - 1) : data}");
+      if (data.length == 1) {
+        data = "";
+      } else if (data.length != 0) {
+        data = data.substring(0, data.length - 1);
+      }
+      
+      var display = "${prompt}";
+      
+      if (data.length > 0) {
+        if (secret) {
+          display += strs.repeat("*", data.length);
+        } else {
+          display += data;
+        }
+      }
+      
+      Console.overwriteLine(display);
       continue;
     }
     
     if (secret) {
-      stdout.write("\x1b[D");
-      stdout.write("*");
+      Console.moveCursorBack();
+      Console.write("*");
     }
     data += char;
   }
   stdin.lineMode = true;
   return data;
+}
+
+class Console {
+  static const String ANSI_ESCAPE = "\x1b[";
+
+  static void moveCursorForward([int times = 1]) => writeANSI("${times}C");
+  static void moveCursorBack([int times = 1]) => writeANSI("${times}D");
+  static void moveCursorUp([int times = 1]) => writeANSI("${times}A");
+  static void moveCursorDown([int times = 1]) => writeANSI("${times}B");
+  static void eraseDisplay([int type = 0]) => writeANSI("${type}J");
+  static void eraseLine([int type = 0]) => writeANSI("${type}K");
+  
+  static void moveToColumn(int number) => writeANSI("${number}G");
+  
+  static void overwriteLine(String line) {
+    moveToColumn(1);
+    eraseLine(2);
+    write(line);
+  }
+  
+  static void nextLine([int times = 1]) => writeANSI("${times}E");
+  static void previousLine([int times = 1]) => writeANSI("${times}F");
+  static void write(String content) => stdout.write(content);
+  static void writeANSI(String after) => stdout.write("${ANSI_ESCAPE}${after}");
 }
