1172194fcce8438e9ea802b40b019652e13a96ab
Kenneth Endfinger <kaendfinger@gmail.com>
2014-08-11 10:34:04 -0400

    Don't use the 'semver' package version API, use pub's API (Adds Constraints)

--- lib/util.dart
+++ lib/util.dart
@@ -1,10 +1,14 @@
 library devtools.util;
 
 import "dart:io";
+import "dart:math";
+
+import "package:collection/equality.dart";
 
 part 'src/util/executables.dart';
 part 'src/util/yaml.dart';
 part 'src/util/projects.dart';
 part 'src/util/io.dart';
 part 'src/util/functions.dart';
-part 'src/util/version.dart';
+part 'src/util/version/base.dart';
+part 'src/util/version/constraint.dart';
