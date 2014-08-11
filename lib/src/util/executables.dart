22abe0e7944436b493de7ceed9e50b14834a624d
Kenneth Endfinger <kaendfinger@gmail.com>
2014-08-11 09:04:45 -0400

    Make findExecutable more Dart-y

--- lib/src/util/executables.dart
+++ lib/src/util/executables.dart
@@ -3,24 +3,21 @@ part of devtools.util;
 typedef ErrorHandler(Exception err);
 typedef NotFoundHandler();
 
-Future<File> findExecutable(String name, {NotFoundHandler notFound: null, ErrorHandler onError: null}) {
-  if (onError == null)
-    onError = (err) {};
-  if (notFound == null)
-    notFound = () {};
-  return new Future(() {
-    String pathVar = Platform.environment["PATH"];
-    if (pathVar == null) {
-      onError(new Exception("PATH Variable is Empty"));
-    }
-    String sep = Platform.isWindows ? ";" : ":";
-    Set<String> PATH = pathVar.split(sep).toSet();
-    for (String path in PATH) {
-      var fullPath = path + Platform.pathSeparator + name;
-      if (fileExists(fullPath)) {
-        return new File(fullPath);
-      }
-    }
-    return null;
-  });
+File findExecutable(String name, {NotFoundHandler notFound: null, ErrorHandler onError: null}) {
+  if (onError == null) onError = (err) {};
+  if (notFound == null) notFound = () {};
+  
+  var pathVar = Platform.environment["PATH"];
+  if (pathVar == null) onError(new Exception("PATH Variable is Empty"));
+  
+  var sep = Platform.isWindows ? ";" : ":";
+  var PATH = pathVar.split(sep).toSet();
+  
+  for (String path in PATH) {
+    var fullPath = path + Platform.pathSeparator + name;
+    var file = new File(fullPath);
+    if (file.existsSync()) return file;
+  }
+  
+  return null;
 }
