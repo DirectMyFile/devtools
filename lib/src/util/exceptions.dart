dc78fade6d0fdb5f62b76102ef763f317d051b66
Kenneth Endfinger <kaendfinger@gmail.com>
2014-08-16 14:34:16 -0400

    Grenades

--- lib/src/util/exceptions.dart
+++ lib/src/util/exceptions.dart
@@ -8,3 +8,12 @@ class Chair {
   @override
   String toString() => message;
 }
+
+class Grenade {
+  final String message;
+  
+  Grenade([this.message = "BAM"]);
+  
+  @override
+  String toString() => message;
+}
