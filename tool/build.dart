7b3560a9d21acf781c8e6bd4e4a430521d21f862
Kenneth Endfinger <kaendfinger@gmail.com>
2014-08-16 21:42:21 -0400

    Fix Bug in Build System

--- tool/build.dart
+++ tool/build.dart
@@ -1,3 +1,5 @@
+#!/usr/bin/env dart
+
 library build;
 
 import 'dart:async';
@@ -15,18 +17,25 @@ part 'config.dart';
 void main(List<String> args) {
   init();
 
-  task("docs", createDocGenTask(".", out_dir: parse_config_value(getvar("docs.output"))));
-  task("analyze", createAnalyzerTask(getvar("analyzer.files").map(parse_config_value)));
+  task("docs", createDocGenTask(".", out_dir: parse_config_value(getvar("docs.output", "out/docs"))));
+  
+  task("analyze", createAnalyzerTask(getvar("analyzer.files", []).map(parse_config_value)));
+  
   task("test", (context) {
     runDartScript(context, getvar("test.file", "test/run.dart"));
   });
+  
   task("version", createVersionTask());
+  
   task("publish", (context) {
     runSdkBinary(context, "pub", arguments: ["publish", "-f"]);
   }, depends: ["version"]);
-  task("check", (_) {}, depends: getvar("check.tasks").map(parse_config_value).toList());
+  
+  task("check", (_) {}, depends: getvar("check.tasks", []).map(parse_config_value).toList());
 
   startGrinder(args);
 }
 
-void task(String name, TaskFunction function, {List<String> depends}) => defineTask(name, taskFunction: function, depends: depends);
+void task(String name, TaskFunction function, {List<String> depends: const []}) {
+  defineTask(name, taskFunction: function, depends: depends);
+}
